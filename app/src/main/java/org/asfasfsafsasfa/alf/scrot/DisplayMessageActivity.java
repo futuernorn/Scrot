f4c4f0eb48c260f20ac45d236ac51c3a874bfb63
Jeffrey Hogan <jeff.hogan1@gmail.com>
2015-01-11 09:27:27 -0600

    Beginning alfs.

--- app/src/main/java/org/asfasfsafsasfa/alf/scrot/DisplayMessageActivity.java
+++ app/src/main/java/org/asfasfsafsasfa/alf/scrot/DisplayMessageActivity.java
@@ -1,9 +1,11 @@
 package org.asfasfsafsasfa.alf.scrot;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -11,13 +13,25 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_display_message, menu);
         return true;
     }
--- app/src/main/java/org/asfasfsafsasfa/alf/scrot/MainActivity.java
+++ app/src/main/java/org/asfasfsafsasfa/alf/scrot/MainActivity.java
@@ -7,8 +7,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "org.asfasfsafsasfa.alf.scrot";
+
+    private Speaker speaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +47,11 @@ public class MainActivity extends ActionBarActivity {
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        TextView textView = (TextView) findViewById(R.id.display_messages);
+        textView.append("\n"+message);
+
+//        textView.setText(message);
     }
 }
